866442f6fed691bac893271574336d6b3123099d
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-05-30 11:28:53 -0700

    Feat: Added Provided HTML to Jamming.
    Used JSX to render HTML in appjs.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,13 +5,13 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <div className="App-playlist" />
+        </div>
       </div>
     );
   }
