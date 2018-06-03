c43d8aebd4dc3c9417aa7ef44df45e0ab218dace
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-06-03 15:22:53 -0700

    Steps 40-48

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,22 +2,42 @@ import React, { Component } from "react";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 import SearchBar from "../SearchBar/SearchBar";
-// import logo from './logo.svg';
+// import logo from './logo.svg'; // <img src={logo} className="App-logo" alt="logo" />
 import "./App.css";
-// <img src={logo} className="App-logo" alt="logo" />
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       searchResults: [
         {
-          name: this.props.track.name,
-          artist: this.props.track.artist,
-          album: this.props.track.album,
+          name: "test",
+          artist: "test",
+          album: "test",
+          id: 1
+        }
+      ],
+      playlistName: "Playlist Time",
+      playlistTracks: [
+        {
+          name: "test2",
+          artist: "test2",
+          album: "test2",
           id: 1
         }
       ]
     };
+    this.addTrack = this.addTrack.bind(this);
+  }
+  addTrack(track) {
+    if (
+      this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)
+    ) {
+      return;
+    } else {
+      let currentTracks = this.state.playlistTracks;
+      currentTracks.push(track);
+      this.setState({ playlistTracks: currentTracks });
+    }
   }
   render() {
     return (
@@ -28,8 +48,14 @@ class App extends Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist" />
-          <SearchResults searchResult={this.state.searchResults} />
-          <Playlist />
+          <SearchResults
+            searchResult={this.state.searchResults}
+            onAdd={this.addTrack}
+          />
+          <Playlist
+            playlistName={this.state.playlistName}
+            playlistTracks={this.state.playlistTracks}
+          />
         </div>
       </div>
     );
